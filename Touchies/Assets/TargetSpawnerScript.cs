cd2e444c9a4c507a1339bc0618bfe5265fe756bd
Kjip <kjipgamer@gmail.com>
2019-06-01 11:42:44 +0200

    Made a summon script

---
+++ Touchies/Assets/TargetSpawnerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSpawnerScript : MonoBehaviour
+{
+    public GameManager gameManager;
+    public GameObject target;
+    GameObject targetParent;
+    Canvas canvas;
+
+    private void Start() {
+        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        targetParent = GameObject.Find("TargetParent");
+        InvokeRepeating("SpawnTarget", 1f, 0.05f);
+        RectTransform objectRectTransform = canvas.GetComponent<RectTransform>();
+        Debug.Log("w: " + objectRectTransform.rect.width + " h: " + objectRectTransform.rect.height);
+    }
+
+    void SpawnTarget() {
+        float xVal = Random.Range(0f, 1081f);
+        float yVal = Random.Range(0f, 1921f);
+
+        Instantiate(target, new Vector2(xVal / 200, yVal / 200), transform.rotation, targetParent.transform);
+    }
+}
